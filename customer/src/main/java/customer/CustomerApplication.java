f600d5bfc8461b10330cba8adf3c6be72e287903
jhon.gonar <jhon.gonar@yandex.com>
2022-04-09 13:56:33 -0500

    Open Feign fully implemented, template classes were moved to centraliez packged and imported where needed

--- customer/src/main/java/customer/CustomerApplication.java
+++ customer/src/main/java/customer/CustomerApplication.java
@@ -3,9 +3,13 @@ package customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients(
+        basePackages = "clients" // I forgot to prefix com.dh on all packages
+)
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
--- customer/src/main/java/customer/CustomerService.java
+++ customer/src/main/java/customer/CustomerService.java
@@ -1,10 +1,13 @@
 package customer;
 
+
+import clients.fraud.FraudCheckResponse;
+import clients.fraud.FraudClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepo customerRepo, RestTemplate restTemplate) {
+public record CustomerService(CustomerRepo customerRepo, RestTemplate restTemplate, FraudClient fraudClient) {
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
         Customer customer = Customer.builder()
                 .firstName(customerRegistrationRequest.firstName())
@@ -12,11 +15,14 @@ public record CustomerService(CustomerRepo customerRepo, RestTemplate restTempla
                 .email(customerRegistrationRequest.email())
                 .build();
         customerRepo.saveAndFlush(customer);//We need to >flush (persist) so we can get an Id for the client on the go
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+        /*FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
                 "http://fraud/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
-        );
+        );*/
+
+        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
+
         if(fraudCheckResponse.isFraudster()){
             throw new IllegalStateException("It's a fruadster");//Not considering null values at the moment
         }
